13c3ce49ea2aca6f9951bc078806320f61d108d7
TrevorMcCormack <mccormackt746@gmail.com>
2024-04-09 16:22:47 -0400

    added footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,25 @@
 import React from "react";
  
 const Home = () => {
+    const footerStyle = {
+        backgroundColor: '#666', 
+        color: '#fff',
+        padding: '50px',
+        textAlign: 'center',
+        position: 'fixed',
+        bottom: '0',
+        width: '100%',
+        boxSizing: 'border-box'
+    };
+
     return (
         <div>
             <h1>Welcome to Online Dealership</h1>
+            <div style={footerStyle}>
+                <p>&copy; 2024 Car Dealership</p>
+            </div>
         </div>
+
     );
 };
  
