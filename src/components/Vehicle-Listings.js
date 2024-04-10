88d53c6d0d86cbcb3435b34a7ec884fd3c2b1961
TrevorMcCormack <mccormackt746@gmail.com>
2024-04-09 20:06:36 -0400

    added a couple of listings

---
+++ src/components/Vehicle-Listings.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Listing = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: baseline;
+    padding: 20px;
+`;
+
+export const BoxForListing = styled.div`
+    position: relative;
+    width: 300px;
+    margin: 20px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+`;
+
