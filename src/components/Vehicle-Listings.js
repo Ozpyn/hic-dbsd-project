c589abdb6d3e4026df508fe3034ea430d17ceb26
TrevorMcCormack <mccormackt746@gmail.com>
2024-04-21 01:36:16 -0400

    changed home page

--- src/components/Vehicle-Listings.js
+++ src/components/Vehicle-Listings.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
 import env from "react-dotenv";
 import Cookies from 'js-cookie';
@@ -7,6 +7,13 @@ import { Box } from '@mui/material';
 
 const apiUrl = env.APIURL;
 
+
+export const MainContainer = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+
 // Section header used for Today's Deals and Used cars 
 export const SectionHeader = styled.h1`
     margin-top: 100px;
@@ -14,12 +21,28 @@ export const SectionHeader = styled.h1`
 `;
 
 
-export const Listing = styled.div`
+export const ListingContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
-    border: 1px solid #ccc;
+    width: 80%; 
     align-items: flex-start;
 `;
+
+
+export const MoreVehiclesButtonContainer = styled.div`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    border: 1px solid #ccc;
+    width: 15%; 
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin-top: 1.35%;    
+    margin-left: -1.2%
+`;
+
 
 // Box to add vehicle + details to
 export const BoxForListing = styled.div`
@@ -39,11 +62,6 @@ export const ButtonContainer = styled.div`
     display: flex;
     justify-content: space-between;
     margin-top: 10px;
-`;
-
-
-export const BoxForMoreVehiclesButton = styled(BoxForListing)`
-    height: ${props => props.height}%; 
 `;
 
 
@@ -256,12 +274,8 @@ export const ListAllVehicles = () => {
     const fetchVehicles = async () => {
     try {
         const response = await axios.get(`${apiUrl}/getAllVehicles`);
-
-
         // Extract the vehicles from the response data
         const { data } = response;
-
-
         setVehicles(data);
     }
     catch (error) {
@@ -271,18 +285,18 @@ export const ListAllVehicles = () => {
    
     useEffect(() => {
         fetchVehicles();
-    }, []); // Empty dependency array ensures the effect runs only once
+    }, []); 
    
     return (
         <div>
-        <h1>Vehicle List</h1>
-        <ul>
-            {vehicles.map(vehicle => (
-            <li key={vehicle.id}>
-                {vehicle.make} {vehicle.model} - {vehicle.year} - {vehicle.vin}
-            </li>
-            ))}
-        </ul>
+            <h1>Vehicle List</h1>
+            <ul>
+                {vehicles.map(vehicle => (
+                <li key={vehicle.id}>
+                    {vehicle.make} {vehicle.model} - {vehicle.year} - {vehicle.vin}
+                </li>
+                ))}
+            </ul>
         </div>
     );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,113 +1,53 @@
 import React from "react";
-import ListingTile, {Listing, BoxForListing, PurchaseButton, SectionHeader, MoreVehiclesButton, SaveVehicleButton, ButtonContainer, ListingContent, BoxForMoreVehiclesButton, ListAllVehicles} from "../components/Vehicle-Listings.js";
+import ListingTile, {MainContainer, ListingContainer, BoxForListing, PurchaseButton, SectionHeader, MoreVehiclesButton, SaveVehicleButton, ButtonContainer, MoreVehiclesButtonContainer} from "../components/Vehicle-Listings.js";
 
 const Home = () => {
-    const TodaysDeals = ['1HGCR2F35FA016731', 'WUADUAFG1CN342714', 'WUADUAFG1CN342714', 'WUADUAFG1CN342714'];
     return (
         <div>
 
             {/* Section off the deals section using div tags*/}
             <div>
                 <h1>Welcome to Online Dealership</h1>
-                <SectionHeader>Today's Deals</SectionHeader>
-            
-                <Listing>
-                    <ListingTile vin={"1HGCR2F35FA016731"} width={15}></ListingTile>
+                <SectionHeader>Best Deals</SectionHeader>
+                <MainContainer>
+                    <ListingContainer>
+                        <ListingTile vin={"1HGCR2F35FA016731"} width={19}/>
 
-                    <ListingTile vin={"WUADUAFG1CN342714"} width={15}></ListingTile>
+                        <ListingTile vin={"WUADUAFG1CN342714"} width={19}/>
 
-                    <ListingTile vin={"ABC123"} width={15}></ListingTile>
+                        <ListingTile vin={"ABC123"} width={19}/>
 
-                    <ListingTile vin={"JHMZE2H31DS895929"} width={15}></ListingTile>
+                        <ListingTile vin={"JHMZE2H31DS895929"} width={19}/>
+                    </ListingContainer>
 
-                    <BoxForMoreVehiclesButton height={50}>
+                    <MoreVehiclesButtonContainer>
                         <MoreVehiclesButton/>
-                    </BoxForMoreVehiclesButton>
+                    </MoreVehiclesButtonContainer>
 
-                </Listing>
+                </MainContainer>
             </div>
 
             {/* separate the used car section from the deals section using different div tags*/ }
             <div>
                 <SectionHeader>Used Cars</SectionHeader>
-                <Listing>
-                <BoxForListing width={15}>
-                        <img src="https://edgecast-img.yahoo.net/mysterio/api/E27CFF5C773BF85E91E9D1129AFCC164333EAB53BB111DEB833624F3CED39C35/autoblog/resizefill_w660_h372;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/commerce/autodata/images/CAC80HOC021B121001.jpg" 
-                            alt="2018 Honda Civic"/> 
 
-                        <a href="/about-vehicle">
-                            <h3>2018 Honda Civic</h3>
-                        </a>
-                        <p>10,000 Miles</p>
-                        <p>$20,000</p>
-                        <p>Other vehicle details we want</p>
+                <MainContainer>
 
-                        <ButtonContainer>
-                            <PurchaseButton/>
-                            <SaveVehicleButton/>
-                        </ButtonContainer>
+                    <ListingContainer>
+                        <ListingTile vin={"1HGCR2F35FA016731"} width={19}/>
 
-                    </BoxForListing>
+                        <ListingTile vin={"1HGCR2F35FA016731"} width={19}/>
 
-                    <BoxForListing width={15}>
-                        <img src="https://edgecast-img.yahoo.net/mysterio/api/E27CFF5C773BF85E91E9D1129AFCC164333EAB53BB111DEB833624F3CED39C35/autoblog/resizefill_w660_h372;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/commerce/autodata/images/CAC80HOC021B121001.jpg" 
-                            alt="2018 Honda Civic"/> 
+                        <ListingTile vin={"1HGCR2F35FA016731"} width={19}/>
 
-                        <a href="/about-vehicle">
-                            <h3>2018 Honda Civic</h3>
-                        </a>
-                        <p>10,000 Miles</p>
-                        <p>$20,000</p>
-                        <p>Other vehicle details we want</p>
+                        <ListingTile vin={"1HGCR2F35FA016731"} width={19}/>
+                    </ListingContainer>
 
-                        <ButtonContainer>
-                            <PurchaseButton/>
-                            <SaveVehicleButton/>
-                        </ButtonContainer>
-
-                    </BoxForListing>
-
-                    <BoxForListing width={15}>
-                        <img src="https://edgecast-img.yahoo.net/mysterio/api/E27CFF5C773BF85E91E9D1129AFCC164333EAB53BB111DEB833624F3CED39C35/autoblog/resizefill_w660_h372;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/commerce/autodata/images/CAC80HOC021B121001.jpg" 
-                            alt="2018 Honda Civic"/> 
-
-                        <a href="/about-vehicle">
-                            <h3>2018 Honda Civic</h3>
-                        </a>
-                        <p>10,000 Miles</p>
-                        <p>$20,000</p>
-                        <p>Other vehicle details we want</p>
-
-                        <ButtonContainer>
-                            <PurchaseButton/>
-                            <SaveVehicleButton/>
-                        </ButtonContainer>
-
-                    </BoxForListing>
-
-                    <BoxForListing width={15}>
-                        <img src="https://edgecast-img.yahoo.net/mysterio/api/E27CFF5C773BF85E91E9D1129AFCC164333EAB53BB111DEB833624F3CED39C35/autoblog/resizefill_w660_h372;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/commerce/autodata/images/CAC80HOC021B121001.jpg" 
-                            alt="2018 Honda Civic"/> 
-
-                        <a href="/about-vehicle">
-                            <h3>2018 Honda Civic</h3>
-                        </a>
-                        <p>10,000 Miles</p>
-                        <p>$20,000</p>
-                        <p>Other vehicle details we want</p>
-
-                        <ButtonContainer>
-                            <PurchaseButton/>
-                            <SaveVehicleButton/>
-                        </ButtonContainer>
-
-                    </BoxForListing>
-
-                    <BoxForListing width={15}>
+                    <MoreVehiclesButtonContainer>
                         <MoreVehiclesButton/>
-                    </BoxForListing>
-
-                </Listing>
+                    </MoreVehiclesButtonContainer>
+                    
+                </MainContainer>
             </div>
         </div>
 
